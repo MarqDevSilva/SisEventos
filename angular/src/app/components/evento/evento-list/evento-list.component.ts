c284b559a031b22bcb67b0245506ada1263a13d2
MarqDevSilva <douglas.tecnico.info@gmail.com>
2023-09-10 10:56:29 -0300

    Componente Inteligente

--- angular/src/app/components/evento/evento-list/evento-list.component.ts
+++ angular/src/app/components/evento/evento-list/evento-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
 import { Evento } from 'src/app/model/evento';
@@ -11,12 +11,14 @@ import { Evento } from 'src/app/model/evento';
 export class EventoListComponent {
 
   @Input() eventos: Evento[] = [];
+  @Output() edit = new  EventEmitter(false);
 
   color: ThemePalette = 'primary';
   mode: ProgressBarMode = 'determinate';
   value = 50;
   bufferValue = 75;
 
-  onEdit(){
+  onEdit(evento: Evento){
+    this.edit.emit(true);
   }
 }
